3e6302addaa83164b682925d7d824b1c5bf8d1f3
Eric Seidel <eric.seidel@devutoo.de>
2019-10-24 15:05:22 +0200

    Updates

---
+++ precache-manifest.eaaa560d75ab24d584cf729f30b46a6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287618e30f94fe5afcbfee26454b35d5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d83a68c3cd3e1bd9b23f",
+    "url": "/monsters-rolodex/static/css/main.206d24d5.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "d83a68c3cd3e1bd9b23f",
+    "url": "/monsters-rolodex/static/js/main.d6ec0305.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
