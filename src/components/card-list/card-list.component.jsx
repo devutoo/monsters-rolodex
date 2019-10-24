52b8b74a377392fef0aa186bfbd52637d92ef4ed
Eric Seidel <eric.seidel@devutoo.de>
2019-10-24 15:06:51 +0200

    adding files for github pages

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Card } from "../card/card.component.jsx";
+
+import "./card-list.styles.css";
+
+export const CardList = props => (
+  <div className="card-list">
+    {/* {props.monsters.map(monster => (
+      <h1 key={monster.id}>{monster.name}</h1>
+    ))} */
+    props.monsters.map(monster => (
+      <Card key={monster.id} monster={monster} />
+    ))}
+  </div>
+);
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./card.styles.css";
+
+export const Card = props => (
+  <div className="card-container">
+    <div className="img-container">
+      <img
+        src={`https://robohash.org/${props.monster.id}?set=set2`}
+        alt="monster"
+      />
+    </div>
+    <h2>{props.monster.name}</h2>
+    <p> {props.monster.email}</p>
+  </div>
+);
